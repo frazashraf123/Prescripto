a7607c46b08c0f17ff56cca18ad82094a1b542a0
Fraz <mallickfraz1234@gmail.com>
2024-10-06 17:24:18 +0530

    implemented login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,57 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+const Login = () => {
+  const { backendUrl, token, setToken } = useContext(AppContext);
+  const navigate = useNavigate();
 
-const Login = () => { 
-  const [state,setState] = useState('Sign Up'); 
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
-  const [name,setName] = useState('');
+  const [state, setState] = useState("Sign Up");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
   const onSubmitHandler = async (event) => {
     event.preventDefault(); // on submission it will not reload the page
-  }
+    try {
+      if (state === "Sign Up") {
+        const { data } = await axios.post(backendUrl + "/api/user/register", {
+          name,
+          password,
+          email,
+        });
+        if (data.success) {
+          localStorage.setItem("token", data.token);
+          setToken(data.token);
+        } else {
+          toast.error(data.message);
+        }
+      } else {
+        const { data } = await axios.post(backendUrl + "/api/user/login", {
+          password,
+          email,
+        });
+        if (data.success) {
+          localStorage.setItem("token", data.token);
+          setToken(data.token);
+        } else {
+          toast.error(data.message);
+        }
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
 
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
         <p className="text-2xl font-semibold">
           {state === "Sign Up" ? "Create Account" : "Login"}
@@ -53,35 +93,36 @@ const Login = () => {
             required
           />
         </div>
-        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+        <button
+          type="submit"
+          className="bg-primary text-white w-full py-2 rounded-md text-base"
+        >
           {state === "Sign Up" ? "Create Account" : "Login"}
         </button>
         {state === "Sign Up" ? (
           <p>
-            Already Have an account?{" "}
+            Already Have an account?
             <span
               onClick={() => setState("Login")}
               className="text-primary underline cursor-pointer"
             >
-              {" "}
-              Login Here{" "}
+              Login Here ?
             </span>
           </p>
         ) : (
           <p>
-            Create a new account?{" "}
+            Create a new account?
             <span
               onClick={() => setState("Sign Up")}
               className="text-primary underline cursor-pointer"
             >
-              {" "}
-              Click Here{" "}
+              Click Here
             </span>
           </p>
         )}
       </div>
     </form>
   );
-}
+};
 
-export default Login
+export default Login;
