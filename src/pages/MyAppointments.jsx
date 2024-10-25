f034c73926f7595022df1f7326917665b3cc2874
Fraz <mallickfraz1234@gmail.com>
2024-10-25 21:36:29 +0530

    enhanced views

--- src/pages/MyAppointments.jsx
+++ src/pages/MyAppointments.jsx
@@ -68,7 +68,7 @@ const MyAppointments = () => {
 
   const initPay = (order) => {
     const options = {
-      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      key: "rzp_test_KIS4vAphUl5PrB",
       amount: order.amount,
       currency: order.currency,
       name: "Appointment Payment",
@@ -151,12 +151,12 @@ const MyAppointments = () => {
             </div>
             <div></div>
             <div className="flex flex-col gap-2 justify-end">
-              {!item.cancelled && item.payment && (
+              {!item.cancelled && item.payment && !item.isCompleted &&  (
                 <button className="sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50">
                   Paid
                 </button>
               )}
-              {!item.cancelled && !item.payment && (
+              {!item.cancelled && !item.payment &&  ! item.isCompleted && (
                 <button
                   onClick={() => appointmentRazorpay(item._id)}
                   className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300"
@@ -164,7 +164,7 @@ const MyAppointments = () => {
                   Pay Online
                 </button>
               )}
-              {!item.cancelled && (
+              {!item.cancelled &&  !item.isCompleted && (
                 <button
                   onClick={() => cancelAppointment(item._id)}
                   className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300"
@@ -172,11 +172,14 @@ const MyAppointments = () => {
                   Cancel Appointment
                 </button>
               )}
-              {item.cancelled && (
+              {item.cancelled && !item.isCompleted &&  (
                 <button className="sm:min-w-48 py-2 border border-red-500 rounded text-red-500">
                   Appointment Cancelled
                 </button>
-              )}
+              )} 
+              {
+                item.isCompleted && <button className="sm:min-w-48 py-2 border-green-500 rounded text-green-500">Completed</button>
+              }
             </div>
           </div>
         ))}
